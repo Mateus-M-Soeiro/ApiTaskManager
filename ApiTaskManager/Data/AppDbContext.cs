2e0a79ce6348bc0bba86fa270024bf514f287e14
Mateus-M-Soeiro <mateusmsoeiro@outlook.com.br>
2026-04-02 17:18:39 -0300

    feat: Setup de entidade User, dbcontext e primeira migration

---
+++ ApiTaskManager/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using ApiTaskManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTaskManager.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
+    }
+}
---
+++ ApiTaskManager/Models/User.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiTaskManager.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(150)]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
